a36c5f2c433fa093f9cf4ba916fa6f2a78b5d934
Mit Desai <mitdesai4703@gmail.com>
2026-03-05 11:28:33 +0530

    commit changes

--- server/configs/db.js
+++ server/configs/db.js
@@ -1,19 +1,34 @@
 import mongoose from "mongoose";
 
+let cached = global.mongoose;
+
+if (!cached) {
+  cached = global.mongoose = { conn: null, promise: null };
+}
+
 const connectDB = async () => {
+  if (cached.conn) {
+    return cached.conn;
+  }
+
+  if (!cached.promise) {
+    const opts = {
+      serverSelectionTimeoutMS: 60000, 
+    };
+
+    cached.promise = mongoose.connect(process.env.MONGODB_URI, opts).then((mongoose) => mongoose);
+  }
+
   try {
-    
+    cached.conn = await cached.promise;
+
     mongoose.connection.on("connected", () => console.log("Database Connected"));
+    mongoose.connection.on("error", (err) => console.error("MongoDB Connection Error:", err));
 
-    
-    await mongoose.connect(process.env.MONGODB_URI, {
-      w: "majority", 
-      
-    });
-
-    console.log("MongoDB connection successful!");
-  } catch (error) {
-    console.error("MongoDB connection error:", error.message);
+    return cached.conn;
+  } catch (err) {
+    console.error("MongoDB connection failed:", err);
+    throw err;
   }
 };
 
